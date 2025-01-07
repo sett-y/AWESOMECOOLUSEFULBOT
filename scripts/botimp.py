f7c986c1c4fa65c64f8c210d639acfa4f82853b6
sett-y <asanagifatal@gmail.com>
2025-01-06 21:25:29 -0800

    intents

--- scripts/botimp.py
+++ scripts/botimp.py
@@ -4,4 +4,4 @@ import discord
 intents = discord.Intents.default()
 intents.message_content = True
 intents.presences = True
-bot = commands.Bot(command_prefix = ">", description = "shut up", intents = intents)
+bot = commands.Bot(command_prefix = [">","bc"], description = "shut up", intents = intents)
