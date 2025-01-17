ac0522d16b27505e4fd904c4c3d440fc02182ff3
sett-y <asanagifatal@gmail.com>
2025-01-16 18:45:41 -0800

    bad word filter

--- scripts/TwitterBot.py
+++ scripts/TwitterBot.py
@@ -1,13 +1,17 @@
 from atproto import AsyncClient, client_utils
 import os
 from scripts.config import bsky_username, bsky_password
-
+import re
 
 client = AsyncClient()
 
 # bluesky demands images/videos to be uploaded as binary blobs
 async def postMessage(message=None, attachment=None, username=None):
     await client.login(login=bsky_username,password=bsky_password)
+    # filter bad words
+    badword = r"n+i+g+e+r+"
+    message = re.sub(badword,"friend",message,flags=re.IGNORECASE)
+    #TODO for loop checking for multiple words
 
     if message and attachment:# text and image/video
         with open(attachment,'rb') as file:
@@ -29,5 +33,10 @@ async def postMessage(message=None, attachment=None, username=None):
 async def getLastPost():
     #profile = 
     await client.login(login=bsky_username,password=bsky_password)
-    bsky_posts = client.get_posts()
+    profile = await client.get_author_feed(actor="awesomecoolbot.bsky.social",limit=1)
+    name = "awesomecoolbot.bsky.social"
+    full_post_uri = profile.feed[0]
+    post_uri = full_post_uri.post.uri.split('post/')[-1]
+    post_url = f"https://bsky.app/profile/{name}/post/{post_uri}"
     
+    return post_url
