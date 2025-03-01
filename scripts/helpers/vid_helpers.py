105bc9762a6103cdd7a03e1bd4c52aced679a2fe
sett-y <asanagifatal@gmail.com>
2025-02-28 20:07:44 -0800

    video helper cmds

--- scripts/helpers/vid_helpers.py
+++ scripts/helpers/vid_helpers.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import urllib.parse
 import os
 import tempfile
+from moviepy import VideoFileClip
 
 
 #TODO: helper functions that check video url, parse, then save and return temp vid path
@@ -13,6 +14,12 @@ async def getVidUrl(ctx: commands.Context, url=None):
             vidUrl = ctx.message.attachments[0].url
             vidName = ctx.message.attachments[0].filename
             print(vidName)
+    elif "http" in ctx.message.content:
+        content = ctx.message.content
+        httpContent = content.split("http")
+        splitContent = httpContent[1].split(" ")
+        vidUrl = "http" + splitContent[0]
+        vidName = "video"
     elif url:
         parsedUrl = urllib.parse.urlparse(url)
         urlPath = parsedUrl.path
@@ -32,8 +39,26 @@ async def getVidUrl(ctx: commands.Context, url=None):
     
     return vidUrl, vidName
 
+async def checkFileSize(session, vidUrl):
+    async with session.head(vidUrl) as response:
+        if response.status == 200:
+            maxSize = 1024 * 1024 * 12 # 12 mb
+            fileSize = int(response.headers.get('Content-Length', 0))
+            if fileSize > maxSize:
+                return True
+            else:
+                return False
+        else:
+            print("could not retrieve file info")
+            return True
+
 async def getTempFile(session, vidUrl):
-    async with session.get() as attach:
+    overMax = await checkFileSize(session, vidUrl)
+    if overMax:
+        print("over max file size")
+        return
+
+    async with session.get(vidUrl) as attach:
         if attach.status != 200:
             print("error while downloading")
             return
@@ -45,6 +70,19 @@ async def getTempFile(session, vidUrl):
                 print(f"Temporary video file created: {tmp_video_path}")
         except Exception as e:
             print(e)
+            return
 
         return tmp_video_path
     
+async def getEditedVideoPath(tmp_video_path):
+    try:
+        with VideoFileClip(tmp_video_path) as editClip:
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmp_output_file:
+                tmp_output_path = tmp_output_file.name
+                editClip.write_videofile(tmp_output_path, audio_codec="aac")
+                print(f"Output video written to temporary file: {tmp_output_path}") 
+                return tmp_output_path    
+    except Exception as e:
+        print(f"Error processing video: {e}")
+        os.remove(tmp_output_path)
+        return
